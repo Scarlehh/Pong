2b41a09f3f6961f14744dfe1026f8c1ca89a4a03
Scarlett <gourleys@tcd.ie>
2019-03-09 21:41:38 +0000

    Add a second paddle and enforce y-axis only movement

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <string.h>
 #include "tonc_input.h"
 #include "tonc_memmap.h"
@@ -13,40 +14,32 @@ const unsigned int pongPal[pongPalLen] = {0xFFFF0000, 0x00000000, 0x00000000, 0x
 										  // Palette 2
 										  0x001F0000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000};
 
-#define pongTilesLen 32
-#define pongTilesBytes pongTilesLen*numBytes
-const unsigned int pongTiles[pongTilesLen] = {0x11112233,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,
-											  0x11110000,};
+#define paddleTilesLen 8
+#define paddleTilesBytes paddleTilesLen*numBytes
+
+const unsigned int leftPaddleTiles[paddleTilesLen] = {0x11110000,
+													  0x11110000,
+													  0x11110000,
+													  0x11110000,
+													  0x11110000,
+													  0x11110000,
+													  0x11110000,
+													  0x11110000,};
+const unsigned int rightPaddleTiles[paddleTilesLen] = {0x00001111,
+													   0x00001111,
+													   0x00001111,
+													   0x00001111,
+													   0x00001111,
+													   0x00001111,
+													   0x00001111,
+													   0x00001111,};
+
+#define numPaddleTiles 32
+
+#define paddleTilePixelLength 32
+#define paddleTilePixelWidth 8
+
+#define xOffset 20
 
 OBJ_ATTR obj_buffer[128];
 OBJ_AFFINE *obj_aff_buffer= (OBJ_AFFINE*)obj_buffer;
@@ -60,25 +53,24 @@ OBJ_AFFINE *obj_aff_buffer= (OBJ_AFFINE*)obj_buffer;
 // L & R shift starting tile
 void obj_test()
 {
-	int x=96, y=32;
-	u32 palette_bank= 0;
+	int y=32;
+	u32 palette_bank=0;
 
-	OBJ_ATTR *paddle= &obj_buffer[0];
-	obj_set_attr(paddle,
+	OBJ_ATTR *paddle_left= &obj_buffer[0];
+	obj_set_attr(paddle_left,
 				 ATTR0_TALL | ATTR0_4BPP,
 				 ATTR1_SIZE_16,
 				 ATTR2_PALBANK(palette_bank));
 
-	// position sprite
-	obj_set_pos(paddle, x, y);
+	OBJ_ATTR *paddle_right= &obj_buffer[1];
+	obj_set_attr(paddle_right,
+				 ATTR0_TALL | ATTR0_4BPP,
+				 ATTR1_SIZE_16,
+				 ATTR2_PALBANK(palette_bank) | numPaddleTiles);
 
-	while(1)
-	{
+	while (1) {
 		vid_vsync();
 		key_poll();
-
-		// move left/right
-		x += 2*key_tri_horz();
 
 		// move up/down
 		y += 2*key_tri_vert();
@@ -86,17 +78,28 @@ void obj_test()
 		// make it glow
 		palette_bank = key_is_down(KEY_SELECT) ? 1 : 0;
 
-		// Build paddle
-		paddle->attr2= ATTR2_BUILD(0, palette_bank, 0);
-		obj_set_pos(paddle, x, y);
+		// Build left paddle
+		paddle_left->attr2= ATTR2_BUILD(0, palette_bank, 0);
+		obj_set_pos(paddle_left, xOffset, y);
+
+		// Build right paddle
+		paddle_right->attr2= ATTR2_BUILD(numPaddleTiles, palette_bank, 0);
+		obj_set_pos(paddle_right, SCREEN_WIDTH-xOffset-paddleTilePixelWidth, y);
 
 		// Updates sprites
-		oam_copy(oam_mem, obj_buffer, 1);
+		oam_copy(oam_mem, obj_buffer, 2);
 	}
 }
 
 int main() {
-	memcpy(&tile_mem[4][0], pongTiles, pongTilesBytes);
+	int i = 0;
+	for (; i < numPaddleTiles; i += 1) {
+		memcpy(tile_mem[4]+i, leftPaddleTiles, paddleTilesBytes);
+	}
+	for (int j = 0; j < numPaddleTiles+i; j += 1) {
+		memcpy(tile_mem[4]+i+j, rightPaddleTiles, paddleTilesBytes);
+	}
+
 	memcpy(pal_obj_mem, pongPal, pongPalBytes);
 
 	oam_init(obj_buffer, 128);
